05544f79b82657bc0057e8f02378ecbf2ca198f2
Oleksandra Polenova <d21121972@gmail.com>
2022-05-03 12:11:50 +0200

    Module 6, task 4 done

---
+++ src/com/goit/hw6/PhraseTest.java
@@ -0,0 +1,33 @@
+package com.goit.hw6;
+
+import java.util.Arrays;
+
+class PhraseTest {
+    public static void main(String[] args) {
+        String[] words = {"alpha", "beta", "gamma"};
+
+        Phrase phrase = new Phrase(words);
+
+        //alpha beta gamma
+        System.out.println(phrase);
+
+        words[0] = "zero";
+
+        //alpha beta gamma
+        System.out.println(phrase);
+    }
+}
+
+class Phrase {
+
+    private final String[] words;
+
+    public Phrase(String[] words) {
+        this.words = Arrays.copyOf(words, words.length);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ", words);
+    }
+}

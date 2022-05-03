1171fdb1533258dbfe20d21d161921db799b8d52
Oleksandra Polenova <d21121972@gmail.com>
2022-05-03 11:39:39 +0200

    Module 6, task 3 done

---
+++ src/com/goit/hw6/GooSearchResultTest.java
@@ -0,0 +1,32 @@
+package com.goit.hw6;
+
+class GooSearchResultTest {
+    public static void main(String[] args) {
+        //test.com
+        System.out.println(new GooSearchResult("https://test.com").parseDomain());
+
+        //apple.in.mars
+        System.out.println(new GooSearchResult("http://apple.in.mars").parseDomain());
+    }
+}
+
+class GooSearchResult {
+
+    private final String url;
+
+    public GooSearchResult(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String parseDomain() {
+        String result = "";
+
+        String[] split = url.split("//");
+        if (split.length > 0) result = split[1].split("/")[0];
+        return result;
+    }
+}

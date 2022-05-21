0e4992e7ab3efde8d60d093be1310ac24eb67f74
Oleksandra Polenova <d21121972@gmail.com>
2022-05-21 22:34:41 +0300

    Module 6 task 10 done

---
+++ src/com/goit/hw6/ShortWordCounterTest.java
@@ -0,0 +1,31 @@
+package com.goit.hw6;
+
+class ShortWordCounterTest {
+    public static void main(String[] args) {
+        //1
+        System.out.println(new ShortWordCounter().count("Java is great language", 2));
+
+        //2
+        System.out.println(new ShortWordCounter().count("Java is great language", 4));
+
+        //3
+        System.out.println(new ShortWordCounter().count("Java is great language", 5));
+
+        //4
+        System.out.println(new ShortWordCounter().count("Java is great language", 100));
+    }
+}
+
+
+class ShortWordCounter {
+
+    public int count(String phrase, int minLength) {
+        int result = 0;
+        String[] words = phrase.split(" ");
+        for (String word : words) {
+            if (word.length() <= minLength)
+                result++;
+        }
+        return result;
+    }
+}
